a15c6a1ca21f7316ab621a8b84a6655d4ab37810
Ernesto <ejcontrerast@outlook.com>
2024-12-11 16:20:11 -0800

    Feat: Supabase

--- src/assets/supabaseClient.ts
+++ src/assets/supabaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kidgnuaeuhjdpconaiur.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZGdudWFldWhqZHBjb25haXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NjA4MTcsImV4cCI6MjA0OTUzNjgxN30.65KAFBKzn4q4EiC2DfsBiJ0evqiE5QoGcnQzabpuDt4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
