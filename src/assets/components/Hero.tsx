179f7c09da4791bc52c56c7466c424b3ff9fd7d2
Ernesto <ejcontrerast@outlook.com>
2024-12-11 16:51:25 -0800

    Feat: previious state

--- src/assets/components/Hero.tsx
+++ src/assets/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import HoverImage from './HoverImage';
 
 const Hero = () => {
   return (
@@ -17,7 +16,11 @@ const Hero = () => {
             <span className="relative z-30 tracking-tighter -ml-1 md:-ml-2">p</span>
             <span className="relative z-20 tracking-tighter -ml-1 md:-ml-2">p</span>
             <span className="relative z-10 tracking-tighter -ml-1 md:-ml-2">y</span>
-            <HoverImage src="static/images/Ball.png" alt="DragonBall ball 4 stars" soundSrc="static/sounds/cumpleanios.mp3" className='cursor-pointer hover:opacity-80 transition-opacity duration-200 w-5 h-5 md:w-16 md:h-16 -ml-3 md:-ml-10 mt-3 md:mt-5 relative z-50'/>
+            <img
+              src="static/images/Ball.png"
+              alt="DragonBall ball"
+              className="w-5 h-5 md:w-16 md:h-16 -ml-3 md:-ml-10 mt-3 md:mt-5 relative z-50"
+            />
             <span className="text-red-500 font-[SayanSansRight] [-webkit-text-stroke:1px_#002f66] md:[-webkit-text-stroke:2px_#002f66] -ml-1 md:-ml-5 -mt-2 md:-mt-3">
               <span className="relative z-[8] tracking-tighter text-5xl md:text-[128px]">b</span>
               <span className="relative z-[7] tracking-tighter -ml-1 md:-ml-2 text-5xl md:text-[120px]">i</span>
